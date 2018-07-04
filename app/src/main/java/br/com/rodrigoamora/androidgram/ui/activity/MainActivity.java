11597977a2c2666155eeb79c4cc04e6f49f135a2
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2018-07-04 00:28:13 -0300

    Adjusts in the NavigationView

--- app/src/main/java/br/com/rodrigoamora/androidgram/ui/activity/MainActivity.java
+++ app/src/main/java/br/com/rodrigoamora/androidgram/ui/activity/MainActivity.java
@@ -75,18 +75,8 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_list_photos) {
             FragmentUtil.changeFragment(R.id.conatiner, ListPhotosInstagramFragment.class, getFragmentManager(), false, null);
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
